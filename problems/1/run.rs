257b8f1dc5685b28732b6f31e91ce857523fa301
Corey Farwell <coreyf@rwell.org>
2013-09-02 16:31:59 -0700

    use int method

--- problems/1/run.rs
+++ problems/1/run.rs
@@ -1,15 +1,13 @@
-use std::*;
-
 fn is_multiple(x: int) -> bool {x % 3 == 0 || x % 5 == 0}
 
-fn work(x: int) -> int {
+fn sum_multiples(x: int) -> int {
    return match x {
       0 => 0,
-      y if is_multiple(y) => y + work(x-1),
-      _ => work(x-1)
+      y if is_multiple(y) => y + sum_multiples(x-1),
+      _ => sum_multiples(x-1)
    }
 }
 
 fn main() {
-   println(int::to_str(work(999)));
+   println(sum_multiples(999).to_str());
 }
